25207cb1e8e730260f500935177b460411b7f013
Boyang Pan <bp141@duke.edu>
2019-02-27 18:38:15 -0500

    hwk5

--- hwk5/main.m
+++ hwk5/main.m
@@ -13,7 +13,6 @@ plotDefaults;
 %% read data
 %read time
 date = xlsread('Problem_Set5.xlsx','Date');
-date_annual = xlsread('Problem_Set5.xlsx','Date_annual');
 
 %read returns
 r_FFF = xlsread('Problem_Set5.xlsx','FFF');
@@ -24,6 +23,7 @@ BEME25.BEME1 = xlsread('Problem_Set5.xlsx','BEME_BEME');
 Mom25.ret = xlsread('Problem_Set5.xlsx','p_212_ret');
 Mom25.size = xlsread('Problem_Set5.xlsx','p_212_size');
 Mom25.pastret = xlsread('Problem_Set5.xlsx','p_212_pastret');
+r_Mom_ext = xlsread('Problem_Set5.xlsx','p_212_extreme');
 
 BEME25.BEME = zeros(1085,25);
 BEME25.BEME(1:6,:) = nan;
@@ -40,12 +40,13 @@ end
 Mom25.size(8:end,:) = Mom25.size(7:end-1,:);
 Mom25.size(7,:) = nan;
 
-BEME25.size(8:end,:) = Mom25.size(7:end-1,:);
-BEME25.size(7,:) = nan;
+BEME25.size(8:end,:) = BEME25.size(7:end-1,:);
+BEME25.size(1:7,:) = nan;
   
 %handle missing value
 r_FFF(r_FFF <= -99) = nan;
 r_BEME_ext(r_BEME_ext <= -99) = nan;
+r_Mom_ext(r_Mom_ext <= -99) = nan;
 BEME25.ret(BEME25.ret <= -99) = nan;
 BEME25.size(BEME25.size <= -99) = nan;
 BEME25.BEME(BEME25.BEME <= -99) = nan;
@@ -58,30 +59,43 @@ BEME25.size = log(BEME25.size);
 BEME25.BEME = log(BEME25.BEME);
 Mom25.size = log(Mom25.size);
 
-clear date_annual
+clear count i 
+
 %% Part 1
 %part a
-mean_FFF = zeros(1,5);
+mean_FFF = zeros(5,5);
 %run the regressions of returns on the the dummy variable
 for i = 1:5
     result = fitlm(r_FFF(:,6),r_FFF(:,i));
-    mean_FFF(i) = result.Coefficients.Estimate(1)+result.Coefficients.Estimate(2);
+    mean_FFF(1,i) = result.Coefficients.Estimate(1);
+    mean_FFF(2,i) = result.Coefficients.tStat(1);
+    mean_FFF(3,i) = result.Coefficients.Estimate(2);
+    mean_FFF(4,i) = result.Coefficients.tStat(2);
+    mean_FFF(5,i) = result.Coefficients.Estimate(1)+result.Coefficients.Estimate(2);
 end
 
 %part b
 %run the regressions of returns on the the dummy variable
-mean_BEME_ext = zeros(1,4);
+mean_BEME_ext = zeros(5,4);
 r_BEME_ext = r_BEME_ext-r_FFF(:,4);
 for i = 1:4
     result = fitlm(r_FFF(:,6),r_BEME_ext(:,i));
-    mean_BEME_ext(i) = result.Coefficients.Estimate(1)+result.Coefficients.Estimate(2);
+    mean_BEME_ext(1,i) = result.Coefficients.Estimate(1);
+    mean_BEME_ext(2,i) = result.Coefficients.tStat(1);
+    mean_BEME_ext(3,i) = result.Coefficients.Estimate(2);
+    mean_BEME_ext(4,i) = result.Coefficients.tStat(2);
+    mean_BEME_ext(5,i) = result.Coefficients.Estimate(1)+result.Coefficients.Estimate(2);
 end
 
-mean_Mom_ext = zeros(1,4);
-r_BEME_ext = r_BEME_ext-r_FFF(:,4);
+mean_Mom_ext = zeros(5,4);
+r_Mom_ext = r_Mom_ext-r_FFF(:,4);
 for i = 1:4
-    result = fitlm(r_FFF(:,6),r_BEME_ext(:,i));
-    mean_BEME_ext(i) = result.Coefficients.Estimate(1)+result.Coefficients.Estimate(2);
+    result = fitlm(r_FFF(:,6),r_Mom_ext(:,i));
+    mean_Mom_ext(1,i) = result.Coefficients.Estimate(1);
+    mean_Mom_ext(2,i) = result.Coefficients.tStat(1);
+    mean_Mom_ext(3,i) = result.Coefficients.Estimate(2);
+    mean_Mom_ext(4,i) = result.Coefficients.tStat(2);
+    mean_Mom_ext(5,i) = result.Coefficients.Estimate(1)+result.Coefficients.Estimate(2);
 end
 
 %% Part 2
@@ -91,7 +105,7 @@ end
    betas_BEME = zeros(4,25);
    intercept = ones(1085,1);
    for i = 1:25
-       betas_BEME(:,i) = regress(BEME25.ret(:,i),[intercept,r_FFF(:,1:3)]);
+       betas_BEME(:,i) = regress(BEME25.ret(:,i)-r_FFF(:,4),[intercept,r_FFF(:,1:3)]);
    end
    betas_BEME = betas_BEME(2:4,:);
    
@@ -104,35 +118,38 @@ end
       gamma_BEME.r1 = zeros(1085,4);
       intercept = ones(25,1);
       for i = 1:1085
-          gamma_BEME.r1(i,:) = (regress(BEME25.ret(i,:)',[intercept,betas_BEME(1,:)',BEME25.size(i,:)',BEME25.BEME(i,:)']))';
+          gamma_BEME.r1(i,:) = (regress((BEME25.ret(i,:)-r_FFF(i,4))',[intercept,betas_BEME(1,:)',BEME25.size(i,:)',BEME25.BEME(i,:)']))';
       end
+      gamma_BEME.r1(gamma_BEME.r1 == 0) = nan;
       
       %equation 2
       gamma_BEME.r2 = zeros(1085,4);
       intercept = ones(25,1);
       for i = 1:1085
-          gamma_BEME.r2(i,:) = (regress(BEME25.ret(i,:)',[intercept,betas_BEME']))';
+          gamma_BEME.r2(i,:) = (regress((BEME25.ret(i,:)-r_FFF(i,4))',[intercept,betas_BEME']))';
       end
+      gamma_BEME.r2(gamma_BEME.r2 == 0) = nan;
       
       %equation 3
       gamma_BEME.r3 = zeros(1085,6);
       intercept = ones(25,1);
       for i = 1:1085
-          gamma_BEME.r3(i,:) = (regress(BEME25.ret(i,:)',[intercept,betas_BEME(1,:)',BEME25.size(i,:)',BEME25.BEME(i,:)',betas_BEME(2:3,:)']))';
+          gamma_BEME.r3(i,:) = (regress((BEME25.ret(i,:)-r_FFF(i,4))',[intercept,betas_BEME(1,:)',BEME25.size(i,:)',BEME25.BEME(i,:)',betas_BEME(2:3,:)']))';
       end
-   
+      gamma_BEME.r3(gamma_BEME.r3 == 0) = nan;
+      
    %part 3
    gamma_BEME.avg1 = zeros(3,4);
    gamma_BEME.avg2 = zeros(3,4);
    gamma_BEME.avg3 = zeros(3,6);
    %mean
-   gamma_BEME.avg1(1,:) = mean(gamma_BEME.r1);
-   gamma_BEME.avg2(1,:) = mean(gamma_BEME.r2);
-   gamma_BEME.avg3(1,:) = mean(gamma_BEME.r3);
+   gamma_BEME.avg1(1,:) = nanmean(gamma_BEME.r1);
+   gamma_BEME.avg2(1,:) = nanmean(gamma_BEME.r2);
+   gamma_BEME.avg3(1,:) = nanmean(gamma_BEME.r3);
    %standard deviation
-   gamma_BEME.avg1(2,:) = std(gamma_BEME.r1)/sqrt(1085);
-   gamma_BEME.avg2(2,:) = std(gamma_BEME.r2)/sqrt(1085);
-   gamma_BEME.avg3(2,:) = std(gamma_BEME.r3)/sqrt(1085);
+   gamma_BEME.avg1(2,:) = nanstd(gamma_BEME.r1)/sqrt(1077);
+   gamma_BEME.avg2(2,:) = nanstd(gamma_BEME.r2)/sqrt(1085);
+   gamma_BEME.avg3(2,:) = nanstd(gamma_BEME.r3)/sqrt(1077);
    %t-stat
    gamma_BEME.avg1(3,:) = gamma_BEME.avg1(1,:)./gamma_BEME.avg1(2,:);
    gamma_BEME.avg2(3,:) = gamma_BEME.avg2(1,:)./gamma_BEME.avg2(2,:);
@@ -150,7 +167,7 @@ index = find(date(:,1) == 1963 & date(:,2) == 1);
    betas_BEME_e = zeros(4,25);
    intercept = ones(1085-index+1,1);
    for i = 1:25
-       betas_BEME_e(:,i) = regress(BEME25.ret(index:end,i),[intercept,r_FFF(index:end,1:3)]);
+       betas_BEME_e(:,i) = regress(BEME25.ret(index:end,i)-r_FFF(index:end,4),[intercept,r_FFF(index:end,1:3)]);
    end
    betas_BEME_e = betas_BEME_e(2:4,:);
    
@@ -161,21 +178,21 @@ index = find(date(:,1) == 1963 & date(:,2) == 1);
       gamma_BEME_e.r1 = zeros(1085-index+1,4);
       intercept = ones(25,1);
       for i = 1:1085-index+1
-          gamma_BEME_e.r1(i,:) = (regress(BEME25.ret(index-1+i,:)',[intercept,betas_BEME_e(1,:)',BEME25.size(i,:)',BEME25.BEME(i,:)']))';
+          gamma_BEME_e.r1(i,:) = (regress((BEME25.ret(index-1+i,:)-r_FFF(index-1+i,4))',[intercept,betas_BEME_e(1,:)',BEME25.size(index-1+i,:)',BEME25.BEME(index-1+i,:)']))';
       end
       
       %equation 2
       gamma_BEME_e.r2 = zeros(1085-index+1,4);
       intercept = ones(25,1);
       for i = 1:1085-index+1
-          gamma_BEME_e.r2(i,:) = (regress(BEME25.ret(index-1+i,:)',[intercept,betas_BEME_e']))';
+          gamma_BEME_e.r2(i,:) = (regress((BEME25.ret(index-1+i,:)-r_FFF(index-1+i,4))',[intercept,betas_BEME_e']))';
       end
       
       %equation 3
       gamma_BEME_e.r3 = zeros(1085-index+1,6);
       intercept = ones(25,1);
       for i = 1:1085-index+1
-          gamma_BEME_e.r3(i,:) = (regress(BEME25.ret(index-1+i,:)',[intercept,betas_BEME_e(1,:)',BEME25.size(i,:)',BEME25.BEME(i,:)',betas_BEME_e(2:3,:)']))';
+          gamma_BEME_e.r3(i,:) = (regress((BEME25.ret(index-1+i,:)-r_FFF(index-1+i,4))',[intercept,betas_BEME_e(1,:)',BEME25.size(index-1+i,:)',BEME25.BEME(index-1+i,:)',betas_BEME_e(2:3,:)']))';
       end
       
    %part 3
@@ -183,13 +200,13 @@ index = find(date(:,1) == 1963 & date(:,2) == 1);
    gamma_BEME_e.avg2 = zeros(3,4);
    gamma_BEME_e.avg3 = zeros(3,6);
    %mean
-   gamma_BEME_e.avg1(1,:) = mean(gamma_BEME_e.r1);
-   gamma_BEME_e.avg2(1,:) = mean(gamma_BEME_e.r2);
-   gamma_BEME_e.avg3(1,:) = mean(gamma_BEME_e.r3);
+   gamma_BEME_e.avg1(1,:) = nanmean(gamma_BEME_e.r1);
+   gamma_BEME_e.avg2(1,:) = nanmean(gamma_BEME_e.r2);
+   gamma_BEME_e.avg3(1,:) = nanmean(gamma_BEME_e.r3);
    %standard deviation
-   gamma_BEME_e.avg1(2,:) = std(gamma_BEME_e.r1)/sqrt(1085);
-   gamma_BEME_e.avg2(2,:) = std(gamma_BEME_e.r2)/sqrt(1085);
-   gamma_BEME_e.avg3(2,:) = std(gamma_BEME_e.r3)/sqrt(1085);
+   gamma_BEME_e.avg1(2,:) = nanstd(gamma_BEME_e.r1)/sqrt(647);
+   gamma_BEME_e.avg2(2,:) = nanstd(gamma_BEME_e.r2)/sqrt(647);
+   gamma_BEME_e.avg3(2,:) = nanstd(gamma_BEME_e.r3)/sqrt(647);
    %t-stat
    gamma_BEME_e.avg1(3,:) = gamma_BEME_e.avg1(1,:)./gamma_BEME_e.avg1(2,:);
    gamma_BEME_e.avg2(3,:) = gamma_BEME_e.avg2(1,:)./gamma_BEME_e.avg2(2,:);
@@ -202,7 +219,7 @@ index = find(date(:,1) == 1963 & date(:,2) == 1);
    betas_Mom = zeros(4,25);
    intercept = ones(1085,1);
    for i = 1:25
-       betas_Mom(:,i) = regress(Mom25.ret(:,i),[intercept,r_FFF(:,1:2),r_FFF(:,5)]);
+       betas_Mom(:,i) = regress(Mom25.ret(:,i)-r_FFF(:,4),[intercept,r_FFF(:,1:2),r_FFF(:,5)]);
    end
    betas_Mom = betas_Mom(2:4,:);
    
@@ -213,21 +230,21 @@ index = find(date(:,1) == 1963 & date(:,2) == 1);
       gamma_Mom.r1 = zeros(1085,4);
       intercept = ones(25,1);
       for i = 1:1085
-          gamma_Mom.r1(i,:) = (regress(Mom25.ret(i,:)',[intercept,betas_Mom(1,:)',Mom25.size(i,:)',Mom25.pastret(i,:)']))';
+          gamma_Mom.r1(i,:) = (regress((Mom25.ret(i,:)-r_FFF(i,4))',[intercept,betas_Mom(1,:)',Mom25.size(i,:)',Mom25.pastret(i,:)']))';
       end
   
       %equation 2
       gamma_Mom.r2 = zeros(1085,4);
       intercept = ones(25,1);
       for i = 1:1085
-          gamma_Mom.r2(i,:) = (regress(Mom25.ret(i,:)',[intercept,betas_Mom']))';
+          gamma_Mom.r2(i,:) = (regress((Mom25.ret(i,:)-r_FFF(i,4))',[intercept,betas_Mom']))';
       end
       
       %equation 3
       gamma_Mom.r3 = zeros(1085,6);
       intercept = ones(25,1);
       for i = 1:1085
-          gamma_Mom.r3(i,:) = (regress(Mom25.ret(i,:)',[intercept,betas_Mom(1,:)',Mom25.size(i,:)',Mom25.pastret(i,:)',betas_Mom(2:3,:)']))';
+          gamma_Mom.r3(i,:) = (regress((Mom25.ret(i,:)-r_FFF(i,4))',[intercept,betas_Mom(1,:)',Mom25.size(i,:)',Mom25.pastret(i,:)',betas_Mom(2:3,:)']))';
       end
       
       %replace gamma = 0 with gamma = nan
@@ -264,7 +281,7 @@ index = find(date(:,1) == 1963 & date(:,2) == 1);
    betas_Mom_h = zeros(4,25);
    intercept = ones(1085-index+1,1);
    for i = 1:25
-       betas_Mom_h(:,i) = regress(Mom25.ret(index:end,i),[intercept,r_FFF(index:end,1:2),r_FFF(index:end,5)]);
+       betas_Mom_h(:,i) = regress(Mom25.ret(index:end,i)-r_FFF(index:end,4),[intercept,r_FFF(index:end,1:2),r_FFF(index:end,5)]);
    end
    betas_Mom_h = betas_Mom_h(2:4,:);
    
@@ -275,21 +292,21 @@ index = find(date(:,1) == 1963 & date(:,2) == 1);
       gamma_Mom_h.r1 = zeros(1085-index+1,4);
       intercept = ones(25,1);
       for i = 1:1085-index+1
-          gamma_Mom_h.r1(i,:) = (regress(Mom25.ret(index-1+i,:)',[intercept,betas_Mom_h(1,:)',Mom25.size(index-1+i,:)',Mom25.pastret(index-1+i,:)']))';
+          gamma_Mom_h.r1(i,:) = (regress((Mom25.ret(index-1+i,:)-r_FFF(index-1+i,4))',[intercept,betas_Mom_h(1,:)',Mom25.size(index-1+i,:)',Mom25.pastret(index-1+i,:)']))';
       end
       
       %equation 2
       gamma_Mom_h.r2 = zeros(1085-index+1,4);
       intercept = ones(25,1);
       for i = 1:1085-index+1
-          gamma_Mom_h.r2(i,:) = (regress(Mom25.ret(index-1+i,:)',[intercept,betas_Mom_h']))';
+          gamma_Mom_h.r2(i,:) = (regress((Mom25.ret(index-1+i,:)-r_FFF(index-1+i,4))',[intercept,betas_Mom_h']))';
       end
       
       %equation 3
       gamma_Mom_h.r3 = zeros(1085-index+1,6);
       intercept = ones(25,1);
       for i = 1:1085-index+1
-          gamma_Mom_h.r3(i,:) = (regress(Mom25.ret(index-1+i,:)',[intercept,betas_Mom_h(1,:)',Mom25.size(index-1+i,:)',Mom25.pastret(index-1+i,:)',betas_Mom_h(2:3,:)']))';
+          gamma_Mom_h.r3(i,:) = (regress((Mom25.ret(index-1+i,:)-r_FFF(index-1+i,4))',[intercept,betas_Mom_h(1,:)',Mom25.size(index-1+i,:)',Mom25.pastret(index-1+i,:)',betas_Mom_h(2:3,:)']))';
       end
       
       %replace gamma = 0 with gamma = nan
@@ -307,9 +324,9 @@ index = find(date(:,1) == 1963 & date(:,2) == 1);
    gamma_Mom_h.avg2(1,:) = nanmean(gamma_Mom_h.r2);
    gamma_Mom_h.avg3(1,:) = nanmean(gamma_Mom_h.r3);
    %standard deviation
-   gamma_Mom_h.avg1(2,:) = nanstd(gamma_Mom_h.r1)/sqrt(length(1085-index+1));
-   gamma_Mom_h.avg2(2,:) = nanstd(gamma_Mom_h.r2)/sqrt(1085-index+1);
-   gamma_Mom_h.avg3(2,:) = nanstd(gamma_Mom_h.r3)/sqrt(1085-index+1);
+   gamma_Mom_h.avg1(2,:) = nanstd(gamma_Mom_h.r1)/sqrt(647);
+   gamma_Mom_h.avg2(2,:) = nanstd(gamma_Mom_h.r2)/sqrt(647);
+   gamma_Mom_h.avg3(2,:) = nanstd(gamma_Mom_h.r3)/sqrt(647);
    %t-stat
    gamma_Mom_h.avg1(3,:) = gamma_Mom_h.avg1(1,:)./gamma_Mom_h.avg1(2,:);
    gamma_Mom_h.avg2(3,:) = gamma_Mom_h.avg2(1,:)./gamma_Mom_h.avg2(2,:);
